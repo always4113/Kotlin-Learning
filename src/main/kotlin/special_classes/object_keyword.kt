135c7cd78f657981b31a0fadc82b5a62a78d14d7
always4113 <always4113@gmail.com>
2023-01-05 22:56:47 +0900

    add several functions to object_keyword.kt & tests

--- src/main/kotlin/special_classes/object_keyword.kt
+++ src/main/kotlin/special_classes/object_keyword.kt
@@ -3,9 +3,38 @@ package special_classes
 import java.util.Random
 
 class LuckDispatcher {
-  fun getNumber() {
+  fun getNumber(): Int {
     val objRandom = Random()
-    println(objRandom.nextInt(90))
-    objRandom.nextInt(90)
+    return objRandom.nextInt(90)
+  }
+}
+
+fun rentPrice(standardDays: Int, festivityDays: Int, specialDays: Int): String {
+
+  val dayRates = object {
+    var standard = 30 * standardDays
+    var festivity = 50 * festivityDays
+    var special = 100 * specialDays
+  }
+
+  val total = dayRates.standard + dayRates.festivity + dayRates.special
+
+  return "Total price: $$total"
+
+}
+
+object DoAuth {
+  fun takeParams(username: String, password: String): String {
+    return "input Auth parameters = $username:$password"
+  }
+}
+
+class BigBen {
+  companion object Bonger {
+    fun getBongs(nTimes: Int): String {
+      val buffer = StringBuffer()
+      for (i in 1..nTimes) buffer.append("BONG ")
+      return buffer.toString()
+    }
   }
 }
--- src/test/kotlin/special_classes/ObjectKeywordKtTest.kt
+++ src/test/kotlin/special_classes/ObjectKeywordKtTest.kt
@@ -1,11 +1,31 @@
 package special_classes
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 internal class ObjectKeywordKtTest {
   @Test
   fun dispatcher() {
-    val d = LuckDispatcher()
-    d.getNumber()
+    val number = LuckDispatcher()
+    number.getNumber()
+  }
+
+  @Test
+  fun rent() {
+    val res = rentPrice(10, 2, 1)
+    assertEquals("Total price: $500", res)
+  }
+
+  @Test
+  fun authentication() {
+    val res = DoAuth.takeParams("always4113", "134340")
+    assertEquals("input Auth parameters = always4113:134340", res)
+  }
+
+  @Test
+  fun bong() {
+    val str = "BONG BONG BONG"
+    val res = BigBen.getBongs(3)
+    assertEquals(str, res.trim())
   }
 }
