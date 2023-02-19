3880b73419387d7b7e6058dac598d256ce6ecffb
always4113 <always4113@gmail.com>
2023-02-19 23:22:30 +0900

    add some tests to GetOrElseKtTest.kt

--- src/main/kotlin/collections/getOrElse.kt
+++ src/main/kotlin/collections/getOrElse.kt
@@ -1,3 +1,5 @@
 package collections
 
 val list = listOf(0, 10, 20)
+
+val getOrElseMap = mutableMapOf<String, Int?>()
--- src/test/kotlin/collections/GetOrElseKtTest.kt
+++ src/test/kotlin/collections/GetOrElseKtTest.kt
@@ -15,4 +15,24 @@ internal class GetOrElseKtTest {
     val res = list.getOrElse(10) { 42 }
     assertEquals(42, res)
   }
+
+  @Test
+  fun getNumber3() {
+    val res = getOrElseMap.getOrElse("x") { 1 }
+    assertEquals(1, res)
+  }
+
+  @Test
+  fun getNumber4() {
+    getOrElseMap["x"] = 3
+    val res = getOrElseMap.getOrElse("x") { 1 }
+    assertEquals(3, res)
+  }
+
+  @Test
+  fun getNumber5() {
+    getOrElseMap["x"] = null
+    val res = getOrElseMap.getOrElse("x") { 1 }
+    assertEquals(1, res)
+  }
 }
